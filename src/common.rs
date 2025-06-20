fb99db6a09a0fe7dcd05c69a99961e9eb8457c00
Islam Bekbuzarov <n157318@gmail.com>
2025-06-20 22:13:20 +0300

    fixed downloader

--- src/common.rs
+++ src/common.rs
@@ -11,25 +11,25 @@ pub fn download_lib_if_needed(lib_dir: impl AsRef<Path>, lib_version: &str) -> a
 
     let lib_file = lib_dir
         .as_ref()
-        .join(format!("libmemtrack_{}.dylib", lib_version));
+        .join(format!("libmemtrace_{}.dylib", lib_version));
 
     if lib_file.exists() {
         return Ok(lib_file.to_str().unwrap().to_string());
     }
 
-    println!("Loading libmemtrack version {}", lib_version);
+    println!("Loading libmemtrace version {}", lib_version);
 
     fs::create_dir_all(lib_dir).context("failed to create dirs")?;
 
     let mut response = reqwest::blocking::get(format!(
-        "https://github.com/blkmlk/memtrack-lib/releases/download/{}/libmemtrack_lib.dylib",
+        "https://github.com/blkmlk/memtrace-lib/releases/download/{}/libmemtrace_lib.dylib",
         lib_version
     ))
-        .context("failed to download libmemtrack.dylib")?;
+        .context("failed to download libmemtrace.dylib")?;
 
     if !response.status().is_success() {
         anyhow::bail!(
-            "failed to download libmemtrack.dylib. status: {}",
+            "failed to download libmemtrace.dylib. status: {}",
             response.status()
         );
     }
@@ -40,7 +40,7 @@ pub fn download_lib_if_needed(lib_dir: impl AsRef<Path>, lib_version: &str) -> a
     io::copy(&mut response, &mut out_file).context("failed to write output file")?;
 
     println!(
-        "Successfully loaded libmemtrack.dylib version {}",
+        "Successfully loaded libmemtrace.dylib version {}",
         lib_version
     );
 
